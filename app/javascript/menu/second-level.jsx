698249c1b927604199c1aad83f1775bc7d851d89
ericgalbarn <ericgalbarn.com>
2025-03-05 14:57:31 +0700

    Comment the whole second level UI components for test

--- app/javascript/menu/second-level.jsx
+++ app/javascript/menu/second-level.jsx
@@ -1,86 +1,92 @@
-import React, { forwardRef } from 'react';
-import PropTypes from 'prop-types';
-import { SideNavItems, SideNavMenu, SideNavMenuItem } from 'carbon-components-react/es/components/UIShell';
-import { itemId, linkProps } from './item-type';
+// import React, { forwardRef } from "react";
+// import PropTypes from "prop-types";
+// import {
+//   SideNavItems,
+//   SideNavMenu,
+//   SideNavMenuItem,
+// } from "carbon-components-react/es/components/UIShell";
+// import { itemId, linkProps } from "./item-type";
 
-const mapItems = (items, hideSecondary, ref) => items.map((item, key) => {
-  const Component = item.items.length ? MenuSection : MenuItem;
+// const mapItems = (items, hideSecondary, ref) =>
+//   items.map((item, key) => {
+//     const Component = item.items.length ? MenuSection : MenuItem;
 
-  return (
-    <Component
-      hideSecondary={hideSecondary}
-      key={item.id}
-      {...item}
-      {...(ref && key === 0 && { ref })}
-    />
-  );
-});
+//     return (
+//       <Component
+//         hideSecondary={hideSecondary}
+//         key={item.id}
+//         {...item}
+//         {...(ref && key === 0 && { ref })}
+//       />
+//     );
+//   });
 
-const MenuItem = forwardRef(({
-  active, href, id, title, type, hideSecondary,
-}, ref) => (
-  <SideNavMenuItem
-    id={itemId(id)}
-    isActive={active}
-    ref={ref}
-    {...linkProps({
-      type, href, id, hideSecondary,
-    })}
-  >
-    {__(title)}
-  </SideNavMenuItem>
-));
+// const MenuItem = forwardRef(
+//   ({ active, href, id, title, type, hideSecondary }, ref) => (
+//     <SideNavMenuItem
+//       id={itemId(id)}
+//       isActive={active}
+//       ref={ref}
+//       {...linkProps({
+//         type,
+//         href,
+//         id,
+//         hideSecondary,
+//       })}
+//     >
+//       {__(title)}
+//     </SideNavMenuItem>
+//   )
+// );
 
-MenuItem.propTypes = {
-  active: PropTypes.bool,
-  hideSecondary: PropTypes.func.isRequired,
-  href: PropTypes.string,
-  id: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired,
-  type: PropTypes.string,
-};
+// MenuItem.propTypes = {
+//   active: PropTypes.bool,
+//   hideSecondary: PropTypes.func.isRequired,
+//   href: PropTypes.string,
+//   id: PropTypes.string.isRequired,
+//   title: PropTypes.string.isRequired,
+//   type: PropTypes.string,
+// };
 
-MenuItem.defaultProps = {
-  active: false,
-  href: undefined,
-  type: 'default',
-};
+// MenuItem.defaultProps = {
+//   active: false,
+//   href: undefined,
+//   type: "default",
+// };
 
-const MenuSection = forwardRef(({
-  active, id, items, title, hideSecondary,
-}, ref) => (
-  <SideNavMenu
-    id={itemId(id, true)}
-    isActive={active}
-    defaultExpanded={active} // autoexpand active section
-    ref={ref}
-    title={__(title)}
-  >
-    {mapItems(items, hideSecondary)}
-  </SideNavMenu>
-));
+// const MenuSection = forwardRef(
+//   ({ active, id, items, title, hideSecondary }, ref) => (
+//     <SideNavMenu
+//       id={itemId(id, true)}
+//       isActive={active}
+//       defaultExpanded={active} // autoexpand active section
+//       ref={ref}
+//       title={__(title)}
+//     >
+//       {mapItems(items, hideSecondary)}
+//     </SideNavMenu>
+//   )
+// );
 
-MenuSection.propTypes = {
-  active: PropTypes.bool,
-  hideSecondary: PropTypes.func.isRequired,
-  id: PropTypes.string.isRequired,
-  items: PropTypes.arrayOf(PropTypes.any).isRequired,
-  title: PropTypes.string.isRequired,
-};
+// MenuSection.propTypes = {
+//   active: PropTypes.bool,
+//   hideSecondary: PropTypes.func.isRequired,
+//   id: PropTypes.string.isRequired,
+//   items: PropTypes.arrayOf(PropTypes.any).isRequired,
+//   title: PropTypes.string.isRequired,
+// };
 
-MenuSection.defaultProps = {
-  active: false,
-};
+// MenuSection.defaultProps = {
+//   active: false,
+// };
 
-const SecondLevel = forwardRef(({ menu, hideSecondary }, ref) => (
-  <SideNavItems>
-    {mapItems(menu, hideSecondary, ref)}
-  </SideNavItems>
-));
+// const SecondLevel = forwardRef(({ menu, hideSecondary }, ref) => (
+//   <SideNavItems>{mapItems(menu, hideSecondary, ref)}</SideNavItems>
+// ));
 
-SecondLevel.propTypes = {
-  menu: PropTypes.any.isRequired,
-  hideSecondary: PropTypes.func.isRequired,
-};
+// SecondLevel.propTypes = {
+//   menu: PropTypes.any.isRequired,
+//   hideSecondary: PropTypes.func.isRequired,
+// };
 
-export default SecondLevel;
+// export default SecondLevel;
