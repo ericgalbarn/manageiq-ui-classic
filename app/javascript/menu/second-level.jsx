9db402655a391414cacf581fca80fb1419c5555b
ericgalbarn <ericgalbarn.com>
2025-03-10 09:45:52 +0700

    Mock test new button

--- app/javascript/menu/second-level.jsx
+++ app/javascript/menu/second-level.jsx
@@ -44,7 +44,7 @@ const MenuItem = forwardRef(
       // >
       //   {__(title)}
       // </SideNavMenuItem>
-      <SideNavMenuItem> New Button</SideNavMenuItem>
+      <SideNavMenuItem href="https://nextjs.org/"> New Button</SideNavMenuItem>
     );
   }
 );
